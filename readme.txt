8e08cc067df9f8d08be46b0951af91b468e368dc
jedouard98 <jedouard98@gmail.com>
2019-02-14 01:27:05 -0500

    Added content for workshop

---
+++ readme.txt
@@ -0,0 +1,16 @@
+Welcome to Makers Valentine Workshop!
+
+The Prologue Software Needed for this--
+
+Arduino IDE: 
+https://www.arduino.cc/en/main/software
+
+ESP8266 Board Manager:
+(On Arduino IDE, Go to File > Preferences, and paste the link below into "Additional Board Manager URLS" near button of page. Then go to Tools, hover mouse over Board, and then click on Board Manager. Type in the search bar "esp8266" and the board should appear. Click install!)
+http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+For Mac Users, driver download:
+https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers
+
+
+
---
+++ workshopCode/workshopCode.ino
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "studiolab-2.4G";
+const char* password = "curlysun894";
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+}
+
+
+void loop()
+{
+  WiFiClient client;
+
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, 80))
+  {
+    Serial.println("connected]");
+
+    Serial.println("[Sending a request]");
+    client.print(String("POST /play") + " HTTP/1.1\r\n" +
+                 "Content-Type: \r\n" +
+                 /* Add your additional headers here! */
+                 "\r\n" +
+                 /* Add your body here! */                 
+                 "letter=l"
+                );
+
+    Serial.println("[Response:]");
+    while (client.connected() || client.available())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  delay(5000);
+}
